005e3360b29629ec0418b911c92e18952672025f
FoJkee <romanovsky0815@gmail.com>
2023-05-24 15:21:48 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -7,61 +7,93 @@ const port = 3008
 
 
 const videos = [
-    {id: 1, title: 'front'},
-    {id: 2, title: "back"}
+    {
+        id: Number(),
+        title: "title",
+        author: "author",
+        canBeDownloaded: Boolean(),
+        minAgeRestriction: null,
+        createdAt: new Date(),
+        publicationDate: new Date(),
+        availableResolutions:[ "P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160"]
 
+    }
 ]
+
+app.delete('/videos/', (req:Request, res:Response) => {
+    res.send('All data is deleted').status(204)
+})
 
 
 app.get('/videos/', (req:Request, res:Response) => {
-    res.send(videos).status(200)
+    res.send(videos)
 })
 
 app.get('/videos/:id', (req:Request, res:Response) => {
-const videosId = videos.find(c => c.id === +req.params.id)
-if(!videosId) {
+const videosGetId = videos.find(p => p.id === +req.params.id)
+if(!videosGetId) {
     res.send(`If video for passed id doesn't exist`).status(404)
     return
 }
-res.send(videosId)
+res.send(videosGetId)
 })
 
  app.delete('/videos/:id', (req:Request, res:Response) => {
-    const videosId = videos.filter(c => c.id !== +req.params.id)
-    res.send(videosId).status(204)
+    const videosDeleteId = videos.filter(c => c.id !== +req.params.id)
+     if(!videosDeleteId){
+        res.send('Not Found').status(404)
+         return
+     }
+
+    res.send(videosDeleteId).status(204)
 })
 
 app.put('/videos/:id', (req:Request, res:Response) => {
    if(!req.body.title){
-       res.send('If the inputModel has incorrect values')
+       res.send({
+           errorsMessages: [
+               {
+                   "message": "string",
+                   "field": "string"
+               }
+           ]
+       })
            .status(400)
        return;
    }
-   const videoUpdate = videos.find(p => p.id === +req.params.id)
-if(!videoUpdate) {
+   const videoPut = videos.find(p => p.id === +req.params.id)
+if(!videoPut) {
     res.send('Not Found')
         .status(404)
     return
 }
-    videoUpdate.title = req.body.title
+    videoPut.title = req.body.title
 res.send('No Content').status(204)
 })
 
 app.post('/videos/', (req:Request, res:Response) => {
     if(!req.body.title) {
-        res.send(`If the inputModel has incorrect values`)
+        res.send({
+            errorsMessages: [
+                {
+                    'message': "string",
+                    'field': "string"
+                }]
+        })
             .status(400)
         return
     }
-    const newVideo = {
-        id: +(new Date()),
+
+    const newVideo:any  = {
+        id: new Date(),
         title: req.body.title,
+        author: '',
     }
     videos.push(newVideo)
+
     res.send('Returns the newly created video')
         .status(201)
 })
-
 
 
 
