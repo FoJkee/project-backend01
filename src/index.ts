811a954df232bf82615b144c70ead846f714e455
FoJkee <romanovsky0815@gmail.com>
2023-05-26 14:36:24 +0300

    fix h01api

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import express, {Request, Response} from 'express'
-import bodyParser from "body-parser";
 import {videosRouter} from "./router/videos-router";
 import {testingRouter} from "./router/testing-router";
 
 const app = express()
 const port = process.env.PORT || 4000
 
-const parserMiddleware = bodyParser()
+const parserMiddleware = express.json()
 
 app.use(parserMiddleware)
+app.use('/videos', videosRouter)
+app.use('/testing', testingRouter)
 
 export const http_statuses = {
     OK_200: 200,
@@ -20,9 +21,6 @@ export const http_statuses = {
 app.get('/', (req: Request, res: Response) => {
     res.send('Hey you!')
 })
-
-app.use('/videos', videosRouter)
-app.use('/testing', testingRouter)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
